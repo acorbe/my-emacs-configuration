b74495a720f6399aa2bae2884c99ece011922e9e
Alessandro Corbetta <corbisoft.codes@gmail.com>
2023-01-30 09:27:27 +0100

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
-THIS_DIR=$(pwd)
+
+#!/usr/bin/env bash
+
+# THIS_DIR=$(pwd)
+# https://stackoverflow.com/a/246128/1714661
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 cd
 #mv .emacs .emacs.prior_install
 rm .emacs
-ln -s $THIS_DIR/literate-version-setup/emacs.el .emacs
+ln -s $SCRIPT_DIR/literate-version-setup/emacs.el .emacs
 mkdir -p .emacs.d
 cd .emacs.d
 rm early-init.el
-ln -s $THIS_DIR/literate-version-setup/early-init.el
-cd $THIS_DIR
+ln -s $SCRIPT_DIR/literate-version-setup/early-init.el
+cd $SCRIPT_DIR
