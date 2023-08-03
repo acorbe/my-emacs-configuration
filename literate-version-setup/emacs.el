34d861d8600f8e7e166c0234a615b67828f4bc38
Alessandro Corbetta <corbisoft.codes@gmail.com>
2023-08-03 13:22:29 +0200

    reinstated org roam

--- literate-version-setup/emacs.el
+++ literate-version-setup/emacs.el
@@ -1,3 +1,8 @@
+;; tries to load the latest org if sraight installed it
+(if (file-directory-p "~/.emacs.d/straight/build/org/")
+    (add-to-list 'load-path "~/.emacs.d/straight/build/org/"))
+
+;; loads org-mode and then configuration as literate 
 (require 'org)
 (org-babel-load-file
  (expand-file-name "emacs_2023.org"
