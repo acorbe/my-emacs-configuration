2c1d8d62045367c281fec73e93cb92beb0cd2210
Alessandro Corbetta <corbisoft.codes@gmail.com>
2023-08-04 11:55:02 +0200

    better dependencies install

--- install-debian-deps.sh
+++ install-debian-deps.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 apt install  -y  build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev
 apt install  -y  libgnutls
 apt install  -y  libjansson-dev
@@ -7,5 +9,6 @@ apt install  -y  libgccjit0
 apt install  -y  libgccjit-12-dev
 
 
-./configure --with-native-compilation=aot --with-libtree-sitter
-
+# ./configure --with-native-compilation=aot --with-libtree-sitter
+echo "emacs configure command:g"
+echo "./configure --with-native-compilation=aot --with-libtree-sitter"
