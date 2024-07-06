2ecde3aad7a2a682abd4746ac99d3e0dd803a27a
Alessandro Corbetta <corbisoft.codes@gmail.com>
2024-07-06 10:57:56 -0400

    update

--- install-debian-deps.sh
+++ install-debian-deps.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 apt install  -y  build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev gcc libpng-dev         libpoppler-dev libpoppler-glib-dev libz-dev make pkg-config
-apt install  -y  libgnutls
+apt install -y  openjdk-17-jdk #  ltex-lsp
+apt install  -y  libgnutls libgnutls-28-dev
 apt install  -y  libjansson-dev
 apt install  -y  librsvg2-dev
 apt install  -y  libtree-sitter-dev
 apt install  -y  libgccjit0
 apt install  -y  libgccjit-12-dev
+apt install  -y  texlive latexmk
 
 
              
