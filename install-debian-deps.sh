a072d393b010721a751a1cedf7364112c750698d
Alessandro Corbetta <corbisoft.codes@gmail.com>
2024-07-07 03:39:21 -0400

    fonts and a path to try nano

--- install-debian-deps.sh
+++ install-debian-deps.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 apt install  -y  build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev gcc libpng-dev         libpoppler-dev libpoppler-glib-dev libz-dev make pkg-config
+
 apt install -y  openjdk-17-jdk #  ltex-lsp
+
 apt install  -y  libgnutls libgnutls-28-dev
-apt install  -y  libjansson-dev
+
+apt install  -y  libjansson-dev # accelerated lsp
 apt install  -y  librsvg2-dev
-apt install  -y  libtree-sitter-dev
+apt install  -y  libtree-sitter-dev 
 apt install  -y  libgccjit0
 apt install  -y  libgccjit-12-dev
 apt install  -y  texlive latexmk
+
+apt install -y fonts-roboto fonts-firacode # fonts e.g. for nano emacs
 
 
              
